9f28cb301cbde9f53815b65a6be63939aaf96080
code-with-AsimKhan <asimkhank9837@gmail.com>
2026-06-15 21:22:09 +0530

    15-06-2026

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express")
 const cors = require('cors')
 const path = require('path')
 
-const corsOptions = {
-    origin: function(origin, callback){
-        const allowedOrigins = ["http://localhost:5173", "http://localhost:8000", "https://apnishop.onrender.com/"];
-        if(!origin || allowedOrigins.includes(origin)){
-            callback(null, true)
-        }else{
-            callback(new Error("You are not Authorized to access This Api"))
-        }
-    },
-    credentials: true,
-}
+// const corsOptions = {
+//     origin: function(origin, callback){
+//         const allowedOrigins = ["http://localhost:5173", "http://localhost:8000", "https://apnishop.onrender.com/"];
+//         if(!origin || allowedOrigins.includes(origin)){
+//             callback(null, true)
+//         }else{
+//             callback(new Error("You are not Authorized to access This Api"))
+//         }
+//     },
+//     credentials: true,
+// }
 
 
 
@@ -21,14 +21,15 @@ require("./db-connect")
 
 const Router = require("./routes/index")
 const app = express()
-app.use(cors(corsOptions))
+// app.use(cors(corsOptions))
+app.use(cors())
 
 app.use(express.json())
-app.use("/public", express.static("./public"))
-app.use(express.static(path.join(__dirname, "dist")))
-
 app.use("/api", Router)
 
+app.use("/public", express.static("./public"))
+
+app.use(express.static(path.join(__dirname, "dist")))
 app.use((req, res) => { 
     express.static(path.join(__dirname, "dist"))
 })
